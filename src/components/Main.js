5d5e8f61fec011eec40d3d3a088957b0c1d665ca
Oleg Khilko <khilkoleg@gmail.com>
2023-03-06 23:13:44 +0300

    feature: fix popups

--- src/components/Main.js
+++ src/components/Main.js
@@ -30,13 +30,11 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
     return (
         <main className="main">
             <section className="profile">
-                <div className="profile__avatar-container">
-                    <button className="profile__avatar-edit-button" type="button" onClick={() => {
-                        onEditAvatar(true)
-                    }}></button>
-                    <img className="profile__avatar" style={{backgroundImage: `url(${userAvatar})`}}
-                         alt="аватар пользователя"/>
-                </div>
+                <button className="profile__avatar-edit-button" type="button" onClick={() => {
+                    onEditAvatar(true)
+                }}><img className="profile__avatar"
+                        style={{backgroundImage: `url(${userAvatar})`}}
+                        /></button>
                 <div className="profile__info">
                     <h1 className="profile__name">{userName}</h1>
                     <button className="profile__edit-button pointer" type="button" onClick={() => {
